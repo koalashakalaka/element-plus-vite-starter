43cda23ec475e53781545b6ce4b5c1a427f969fb
Renderz <464816158@qq.com>
2022-03-09 17:34:34 +0800

    feat: add vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,60 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfig } from 'vite';
+
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+// import legacy from '@vitejs/plugin-legacy';
+import vueSetupExtend from 'vite-plugin-vue-setup-extend';
 
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const pathSrc = path.resolve(__dirname, 'src');
 
+/**
+ * Whether to generate package preview
+ */
+export function isReportMode(): boolean {
+  return process.env.REPORT === 'true';
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${pathSrc}/`,
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
+export default ({}: ConfigEnv): UserConfig => {
+  return {
+    resolve: {
+      alias: {
+        '~/': `${pathSrc}/`,
       },
     },
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
-        }),
-      ],
-      dts: path.resolve(pathSrc, 'components.d.ts'),
-    }),
-  ],
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/styles/element/index.scss" as *;`,
+        },
+      },
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueSetupExtend(),
+      // legacy(),
+      Components({
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+          }),
+        ],
+        dts: path.resolve(pathSrc, 'components.d.ts'),
+      }),
+      visualizer(
+        isReportMode() && {
+          filename: './node_modules/.cache/visualizer/stats.html',
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        },
+      ),
+    ],
+  };
+};
